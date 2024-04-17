e2b8cd583be471cda2c488df30a399e3ec4b1741
PRATIKSHA DUBEY <132836808+pratiksha259@users.noreply.github.com>
2024-04-17 19:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (30.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -7,43 +7,28 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
-// class Solution {
-//     public ListNode removeNthFromEnd(ListNode head, int n) {
-        
-//     }
-// }
+ **/
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int len = 0;
-        ListNode temp = head;
         
-        // Step 1: Traverse the linked list to find its length
-        while(temp != null) {
-            len++;
-            temp = temp.next;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(n-- > 0) {
+            fast = fast.next;
         }
-        
-        // Reset temp to the head of the list
-        temp = head;
-        ListNode prev = null;
-        int pos = len - n;
-        
-        // Special case: If the node to be removed is the head itself
-        if(pos == 0) return head.next;
-        
-        // Step 2: Traverse the list again to reach the node just before the one to be removed
-        for(int i = 0; i < pos; i++) {
-            prev = temp;
-            temp = temp.next;
+
+        if(fast == null) {
+            return head.next;
         }
-        
-        // Step 3: Update the pointers to remove the node
-        prev.next = temp.next;
-        
-        // Step 4: Delete the removed node
-        temp = null;
-        
+
+        while(fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
         return head;
     }
 }
+        
