40d825ec43ea51dcaa5a51ea0e7864c1e35e1a8a
PRATIKSHA DUBEY <132836808+pratiksha259@users.noreply.github.com>
2024-07-06 20:22:28 +0530

    Time: 1 ms (99.84%) | Memory: 48.3 MB (31.08%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,43 +10,45 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
+        ListNode mid;
         ListNode slow=head;
         ListNode fast=head.next;
-        while(fast!=null&&fast.next!=null)
+        while(fast!=null && fast.next!=null)
         {
             slow=slow.next;
             fast=fast.next.next;
         }
-        ListNode mid=slow;
-        //reverse
-        ListNode curr=mid.next;
-        mid.next=null;
-        ListNode prev=null;
-        ListNode next;
-        while(curr!=null)
-        {
+        mid=slow;
+        
+
+         ListNode curr=mid.next;
+         mid.next=null;
+         ListNode prev=null;
+         ListNode next;
+         while(curr!=null)
+         {
             next=curr.next;
             curr.next=prev;
             prev=curr;
             curr=next;
-        }
-        ListNode lh=head;
-        ListNode rh=prev;
-        //alt merge
-        ListNode nextl;
-        ListNode nextr;
-        while(lh!=null&&rh!=null)
-        {
-            nextl=lh.next;
-            lh.next=rh;
-            nextr=rh.next;
-            rh.next=nextl;
-            //update
-            lh=nextl;
-            rh=nextr;
+         }
 
+         ListNode r=prev;
+         ListNode l=head;
+         ListNode nextl;
+         ListNode nextr;
+         while(l!=null && r!=null)
+         {
+           nextl=l.next;
+           l.next=r;
+           nextr=r.next;
+           r.next=nextl;
            
+           l=nextl;
+           r=nextr;
+         }
+         return;
 
-        }
+        
     }
 }
