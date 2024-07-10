2e2df90f2959611b4cc0f5470f9d73baee02e3c3
PRATIKSHA DUBEY <132836808+pratiksha259@users.noreply.github.com>
2024-07-10 11:57:36 +0530

    Time: 2 ms (86.31%) | Memory: 55.3 MB (58.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,16 +1,34 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-    int n=nums.length;
-        int p=1,s=1;
-        int arr2[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr2[i]=p;
-            p=p*nums[i];
+        int[] a = new int[nums.length];
+        int p = 1;
+        int c = 0;
+        
+        // Calculate the product of all non-zero elements and count the zeros
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                p = p * nums[i];
+            } else {
+                c++;
+            }
         }
-        for(int i=n-1;i>=0;i--){
-            arr2[i]=arr2[i]*s;
-            s=s*nums[i];
+        
+        // If more than one zero, all elements in the result are zero
+        if (c > 1) {
+            return a; // all elements in 'a' are already zero
         }
-        return arr2;
+        
+        // Calculate the result array based on the zero count
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 && c == 0) {
+                a[i] = p / nums[i];
+            } else if (nums[i] == 0) {
+                a[i] = p; // when there is exactly one zero, only that position gets the product
+            } else {
+                a[i] = 0; // if there's one zero in the array, all other positions are zero
+            }
+        }
+        
+        return a;
     }
-    }
+}
