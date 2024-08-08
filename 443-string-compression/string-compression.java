59a1a64acc9181ffbcdac9cdc38990d3fa8eacce
PRATIKSHA DUBEY <132836808+pratiksha259@users.noreply.github.com>
2024-08-08 11:17:01 +0530

    Time: 2 ms (29.11%) | Memory: 43.8 MB (59.21%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,10 +1,6 @@
 class Solution {
     public int compress(char[] chars) {
-        HashMap<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<chars.length;i++)
-        {
-            hm.put(chars[i],hm.getOrDefault(chars[i],0)+1);
-        }
+       
         int c=1;
         StringBuilder sb=new StringBuilder("");
         for(int i=1;i<chars.length;i++)
